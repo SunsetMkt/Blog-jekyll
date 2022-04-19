100210d1892e5e735e0e483f5f32add180d09d74
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2022-04-19 20:53:55 +0800

    Activate Google Translate

---
+++ assets/js/custom-script-loads-after-footer.js
@@ -0,0 +1,14 @@
+// Activate Google Translate
+var lang = (navigator.language || navigator.browserLanguage).toLowerCase();
+if (lang != 'zh-cn' || /translate=true/.test(window.location)) {
+    document.write('<scr' + 'ipt src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" defer="defer">' + '</scr' + 'ipt>');
+    var google_translate_element = document.createElement('div');
+    google_translate_element.innerHTML = '<div id="google_translate_element"></div>';
+    document.getElementsByTagName('body')[0].appendChild(google_translate_element);
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+            pageLanguage: 'zh-CN',
+            layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+        }, 'google_translate_element');
+    }
+}
