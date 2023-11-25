9eb9dc2026844c57b486504f7b4ca75b9bbafc91
Sunset Mikoto <26019675+lwd-temp@users.noreply.github.com>
2023-11-25 22:34:34 +0800

    Lorem ipsum

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -300,6 +300,7 @@ if (document.querySelector(".site-subtitle")) {
     "NaN",
     "null",
     "「在需要的时候，你要能够为自己挺身而出。」",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     // "「」",
   ];
   // random
