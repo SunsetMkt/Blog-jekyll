c4b2e2ada37149deaa5a1297e0a76ec781fcb88b
Sunset Mikoto <26019675+lwd-temp@users.noreply.github.com>
2023-11-28 21:35:09 +0800

    update

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -301,6 +301,7 @@ if (document.querySelector(".site-subtitle")) {
     "null",
     "「在需要的时候，你要能够为自己挺身而出。」",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "「能解释为愚蠢的，就不要解释为恶意。」",
     // "「」",
   ];
   // random
