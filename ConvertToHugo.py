84bae92ea4bcf55e0047d6a7dce6e7ed56e58c78
Sunset <26019675+SunsetMkt@users.noreply.github.com>
2024-11-11 22:12:23 +0800

    u

---
+++ ConvertToHugo.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+
+POSTS_PATH = "_posts/"
+RES_PATH = "assets/postres/"
+OUT_PATH = "out/"
+
+
+def list_dir(path):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            yield os.path.join(root, file)
+
+
+def get_filename(path):
+    return path.split("/")[-1]
+
+
+def remove_extension(filename):
+    return filename.split(".")[0]
+
+
+def mkdir(path):
+    os.makedirs(path, exist_ok=True)
+
+
+def main():
+    for path in list_dir(POSTS_PATH):
+        filename = remove_extension(get_filename(path))
+        title = remove_extension("".join(filename[11:]))
+        respath = os.path.join(RES_PATH, filename)
+        outpath = os.path.join(OUT_PATH, title)
+        mkdir(outpath)
+        # Check if respath is a dir
+        if os.path.isdir(respath):
+            # Copy respath to out
+            shutil.copytree(respath, outpath, dirs_exist_ok=True)
+        cp = shutil.copyfile(path, os.path.join(outpath + "/index.md"))
+
+
+if __name__ == "__main__":
+    main()
